35b59c88457cbae4698d56d06a5e32aee0cfee7c
caio367 <caio.antuns@gmail.com>
2021-05-30 13:06:17 -0300

    Ajustando html com js
    Ajuste geral para funcionamento correto da validação

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-function validaCPF(cpf); {
+// testando uma forma de desativar a caixa do HTML5 required
+// document.addEventListener('invalid', (
+// function () {
+//     return function (e) {
+//     e.preventDefault();
+//     document.getElementById("cpf_digitado").focus();
+//     };
+// }
+// )(), true);
+
+
+
+// vai aparecer um alerta na tela, porque vou chamar a função alert do javascript:
+
+function validaCPF(cpf) {
+    document.getElementById('accept').style.display = 'none';
+    document.getElementById('fail').style.display = 'none';
+    document.getElementById('incompleto').style.display = 'none';
+    document.getElementById('invalido').style.display = 'none';
+
+
+
     if (cpf.length != 11) {
+        document.getElementById('incompleto').style.display = 'block';
         return false;
     }
     else {
@@ -7,34 +29,68 @@ function validaCPF(cpf); {
         var numeros = cpf.substring(0, 9);
         var digitos = cpf.substring(9);
 
+        console.log("Numeros", numeros);
+        console.log("Digitos", digitos);
 
         var soma = 0;
         for (var i = 10; i > 1; i--) {
+            console.log("Abacate");
+            console.log(typeof (numeros.charAt(10 - i)), numeros.charAt(10 - i));
+            console.log(typeof (i), i);
             soma += numeros.charAt(10 - i) * i;
         }
 
 
-        var resultado = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+        console.log(soma);
+
+        var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        console.log("RESULTADO", resultado);
+        console.log("1º Digito", digitos.charAt(0) * 1);
+        console.log("Tipo - 1º Digito", typeof (digitos.charAt(0) * 1));
 
         // validação do primeiro digito
-        if (resultado != digitos.charAt(0)) {
+        if (resultado != digitos.charAt(0) * 1) {
+            console.log("è diferente");
+            document.getElementById('invalido').style.display = 'block';
             return false;
 
         }
+
+        console.log(
+            digitos.toString().charAt(0) + ' é a primeira posição da variavel soma'
+        );
 
         soma = 0;
+
+
         numeros = cpf.substring(0, 10);
+        console.log("Numeros", numeros);
 
         for (var k = 11; k > 1; k--) {
+            console.log("Maça");
+            console.log(typeof (numeros.charAt(11 - k)), numeros.charAt(11 - k));
+            console.log(typeof (k), k);
             soma += numeros.charAt(11 - k) * k;
         }
+        console.log(soma);
 
-        resultado = soma % 11 > 2 ? 0 : 11 - (soma % 11);
+        resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        console.log("RESULTADO", resultado);
+        console.log("2º Digito", digitos.charAt(1) * 1);
+        console.log("Tipo - 2º Digito", typeof (digitos.charAt(1) * 1));
+
 
         //Validação do segundo digito
-        if (resultado != digitos.charAt(1)); {
+        if (resultado != digitos.charAt(1) * 1) {
+            document.getElementById('invalido').style.display = 'block';
             return false;
         }
+
+        console.log("Valido Válido!");
+
+
         return true;
     }
 
@@ -42,6 +98,7 @@ function validaCPF(cpf); {
 
 function validacao() {
     console.log('Iniciando validação CPF');
+
     document.getElementById('accept').style.display = 'none';
     document.getElementById('fail').style.display = 'none';
 
@@ -49,35 +106,34 @@ function validacao() {
 
     var resultadovalidacao = validaCPF(cpf);
 
-    if (resultadoValidacao == true) {      // mesmo se não colocar == true, o programa entende
+    if (resultadovalidacao == true) {      // mesmo se não colocar == true, o programa entende
         document.getElementById('accept').style.display = 'block';
     }
     else {
         document.getElementById('fail').style.display = 'block';
     }
 
+    return resultadovalidacao
 
 
 }
 function validateForm() {
 
-    checkvalidation = false //true
+    checkvalidation = false //Esse é o correto -> true
 
     //Padrões UI
     var erro_obrigatorio = "Campo Obrigatório*"
     var erro_style = "red"
 
     // Objeto principal
-    var pessoa = {} // criando um objeto Json
+    var pessoa = {} // criando um objeto 
 
 
     // Capturando Dados
     // Capturando valor do input id=nome Inputsobrenome
     pessoa["nome"] = document.forms["cadastroForm"]["Inputnome"].value;
     pessoa["sobrenome"] = document.forms["cadastroForm"]["Inputsobrenome"].value;
-    //
-    // cpf
-    //
+    pessoa["cpf"] = document.forms["cadastroForm"]["cpf_digitado"].value
     pessoa["celular"] = document.forms["cadastroForm"]["Inputcelular"].value;
     pessoa["rua"] = document.forms["cadastroForm"]["Inputrua"].value;
     pessoa["numero"] = document.forms["cadastroForm"]["Inputnumero"].value;
@@ -85,7 +141,7 @@ function validateForm() {
     // Captura de Labels - erros
     var labelnome = document.getElementById("labelnome");
     var labelsobrenome = document.getElementById("labelsobrenome");
-    //cpf
+    var labelcpf = document.getElementById("labelcpf");
     var labelcelular = document.getElementById("labelcelular");
     var labelrua = document.getElementById("labelrua");
     var labelnumero = document.getElementById("labelnumero");
@@ -110,9 +166,16 @@ function validateForm() {
         checkvalidation = false
     }
 
-    // Validação CPF
-    ////
-    ///
+
+    //Validação CPF
+
+    if (pessoa["cpf"]) {
+        checkvalidation = validacao()
+    } else {
+        labelcpf.innerText = erro_obrigatorio
+        labelcpf.style.color = erro_style
+        checkvalidation = false
+    }
 
     if (pessoa["celular"]) {
 
@@ -137,6 +200,9 @@ function validateForm() {
         labelnumero.style.color = erro_style
         checkvalidation = false
     }
+
+
+
 
 
     //Validação Email
